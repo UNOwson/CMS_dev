a90be39f8a8b93601bf20106d930e05396794a9e
coolternet <coolternet@live.ca>
2016-02-06 21:03:59 -0500

    Implantation du systeme de langue pour l'installation / changement de version

--- evo/version.php
+++ evo/version.php
@@ -2,10 +2,10 @@
 define('EVO_VERSION', '0.8-dev'); //Suffix: dev < alpha = a < beta = b < RC = rc < nothing < # < pl = p
 define('EVO_RELEASEDATE', '');
 
-define('EVO_BUILD', '50a3441');
-define('EVO_BUILDDATE', 'Sat Feb 6 12:41:51 2016 -0500');
+define('EVO_BUILD', '26901f5');
+define('EVO_BUILDDATE', 'Sat Feb 6 20:58:59 2016 -0500');
 
-define('EVO_REVISION', 210);
-define('EVO_REVDATE', 'Sat Feb 6 12:41:51 2016 -0500');
+define('EVO_REVISION', 213);
+define('EVO_REVDATE', 'Sat Feb 6 20:58:59 2016 -0500');
 
 define('DATABASE_VERSION', 3);
